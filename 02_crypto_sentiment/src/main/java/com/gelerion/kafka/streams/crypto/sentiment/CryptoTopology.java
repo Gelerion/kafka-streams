641b88109553f96747dc472bbd5fd4c034528f17
Gelerion <denis.shuvalov1@gmail.com>
2022-06-27 10:04:41 +0300

    [02] crypto-sentiment -- branching data

--- 02_crypto_sentiment/src/main/java/com/gelerion/kafka/streams/crypto/sentiment/CryptoTopology.java
+++ 02_crypto_sentiment/src/main/java/com/gelerion/kafka/streams/crypto/sentiment/CryptoTopology.java
@@ -7,6 +7,7 @@ import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Predicate;
 import org.apache.kafka.streams.kstream.Printed;
 
 public class CryptoTopology {
@@ -21,6 +22,29 @@ public class CryptoTopology {
         // The print operator allows us to easily view data as it flows through our application.
         // It is generally recommended for development use only
         stream.print(Printed.<byte[], Tweet>toSysOut().withLabel("tweets-stream"));
+
+        // Filter retweets
+        KStream<byte[], Tweet> filtered = stream.filterNot((key, tweet) -> tweet.isRetweet());
+
+        // Branching Data
+        // Branching is typically required when events need to be routed to different stream processing steps
+        // or output topics based on some attribute of the event itself.
+        Predicate<byte[], Tweet> englishTweets = (key, tweet) -> tweet.getLang().equals("en");
+        Predicate<byte[], Tweet> nonEnglishTweets = (key, tweet) -> !tweet.getLang().equals("en");
+
+        // Now that we have defined our branching conditions, we can leverage Kafka Streams’ branch operator,
+        // which accepts one or more predicates and returns a list of output streams that correspond to each predicate.
+        // Note that each predicate is evaluated in order, and a record can only be added to a single branch.
+        // If a record doesn’t match any predicate, then it will be dropped:
+        KStream<byte[], Tweet>[] branches = filtered.branch(englishTweets, nonEnglishTweets);
+
+        // English tweets
+        KStream<byte[], Tweet> englishStream = branches[0];
+        englishStream.print(Printed.<byte[], Tweet>toSysOut().withLabel("tweets-english"));
+
+        // non-English tweets
+        KStream<byte[], Tweet> nonEnglishStream = branches[1];
+        nonEnglishStream.print(Printed.<byte[], Tweet>toSysOut().withLabel("tweets-non-english"));
 
         return builder.build();
     }
