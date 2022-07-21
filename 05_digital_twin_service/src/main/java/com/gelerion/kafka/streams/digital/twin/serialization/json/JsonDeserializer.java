13e76b3b31cc507080e6c0149107b6678ad892c0
Gelerion <denis.shuvalov1@gmail.com>
2022-07-21 17:54:28 +0300

    [05] digital_twin_service -- models & json serdes

---
+++ 05_digital_twin_service/src/main/java/com/gelerion/kafka/streams/digital/twin/serialization/json/JsonDeserializer.java
@@ -0,0 +1,39 @@
+package com.gelerion.kafka.streams.digital.twin.serialization.json;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.kafka.common.serialization.Deserializer;
+
+import java.lang.reflect.Type;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class JsonDeserializer<T> implements Deserializer<T> {
+    private final Gson gson =
+            new GsonBuilder()
+                    .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                    .create();
+
+    private Class<T> destinationClass;
+    private Type reflectionTypeToken;
+
+    /** Default constructor needed by Kafka */
+    public JsonDeserializer(Class<T> destinationClass) {
+        this.destinationClass = destinationClass;
+    }
+
+    public JsonDeserializer(Type reflectionTypeToken) {
+        this.reflectionTypeToken = reflectionTypeToken;
+    }
+
+    @Override
+    public T deserialize(String topic, byte[] data) {
+        if (data == null) {
+            return null;
+        }
+
+        Type type = destinationClass != null ? destinationClass : reflectionTypeToken;
+        return gson.fromJson(new String(data, UTF_8), type);
+    }
+}
---
+++ 05_digital_twin_service/src/main/java/com/gelerion/kafka/streams/digital/twin/serialization/json/JsonSerializer.java
@@ -0,0 +1,23 @@
+package com.gelerion.kafka.streams.digital.twin.serialization.json;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.kafka.common.serialization.Serializer;
+
+import java.nio.charset.StandardCharsets;
+
+class JsonSerializer<T> implements Serializer<T> {
+    private final Gson gson =
+            new GsonBuilder()
+                    .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                    .create();
+
+    /** Default constructor needed by Kafka */
+    public JsonSerializer() {}
+
+    @Override
+    public byte[] serialize(String topic, T type) {
+        return gson.toJson(type).getBytes(StandardCharsets.UTF_8);
+    }
+}
